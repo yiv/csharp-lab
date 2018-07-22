0d7716554fb65fa1ccfb147de31abe0bf87f15e4
yiv <yuefengshao@gmail.com>
2018-07-22 14:52:44 +0800

    new

---
+++ http/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace http
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            await HttpGet();
+            Console.WriteLine("Hello World2!");
+            Console.ReadKey();
+        }
+        static async Task HttpGet() {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync("http://www.baidu.com/");
+            if (response.IsSuccessStatusCode) {
+                Console.WriteLine($"Response Status Code: {(int)response.StatusCode} {response.ReasonPhrase}");
+                string responseBodyAsText = await response.Content.ReadAsStringAsync();
+                Console.WriteLine($"Received payload of {responseBodyAsText.Length} characters");
+                Console.WriteLine();
+                Console.WriteLine(responseBodyAsText);
+            }
+
+        }
+    }
+}
---
+++ rc4/Program.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Text;
+
+namespace rc4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Encoding encoding = Encoding.UTF8;
+            var bytes = RC4.Encrypt("中", "f63dfeafe6bd2f74fedcf754c89d25ad", encoding);
+            //Console.WriteLine("hahahahahah");
+            Console.Write(Convert.ToBase64String(bytes));
+            Console.ReadKey();
+        }
+    }
+    public class RC4 {
+        private byte[] keybox;
+        private const int keyLen = 256;
+        private Encoding encoding;
+
+        public RC4(string pass) {
+            if (string.IsNullOrEmpty(pass)) throw new ArgumentNullException("pass");
+            var ps = Encoding.UTF8.GetBytes(pass);
+            encoding = Encoding.UTF8;
+            keybox = GetKey(ps, keyLen);
+        }
+
+        public RC4(string pass, Encoding encoding) {
+            if (string.IsNullOrEmpty(pass)) throw new ArgumentNullException("pass");
+            var ps = encoding.GetBytes(pass);
+            this.encoding = encoding;
+            keybox = GetKey(ps, keyLen);
+        }
+
+        /// <summary>
+        /// Encrypt
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public byte[] Encrypt(string data) {
+            if (string.IsNullOrEmpty(data)) throw new ArgumentNullException("data");
+            return encrypt(encoding.GetBytes(data));
+        }
+        /// <summary>
+        /// Encrypt
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="encoding"></param>
+        /// <returns></returns>
+        public byte[] Encrypt(string data, Encoding encoding) {
+            if (string.IsNullOrEmpty(data)) throw new ArgumentNullException("data");
+            return encrypt(encoding.GetBytes(data));
+        }
+        /// <summary>
+        /// Encrypt
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public byte[] Encrypt(byte[] data) {
+            if (data == null) throw new ArgumentNullException("data");
+            if (data.Length == 0) throw new ArgumentNullException("data");
+            return encrypt(data);
+        }
+        private byte[] encrypt(byte[] data) {
+            byte[] mBox = new byte[keyLen];
+            //Buffer.BlockCopy(keybox, 0, mBox, 0, keyLen);
+
+            Array.Copy(keybox, mBox, keyLen);
+            byte[] output = new byte[data.Length];
+            int i = 0, j = 0;
+            for (Int64 offset = 0; offset < data.Length; offset++) {
+                i = (++i) & 0xFF;
+                j = (j + mBox[i]) & 0xFF;
+                byte temp = mBox[i];
+                mBox[i] = mBox[j];
+                mBox[j] = temp;
+
+                byte a = data[offset];
+                byte b = mBox[(mBox[i] + mBox[j]) & 0xFF];
+                output[offset] = (byte)((int)a ^ (int)b);
+            }
+            return output;
+        }
+
+
+        /// <summary>
+        /// Encrypt
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="pass"></param>
+        /// <param name="encoding"></param>
+        /// <returns></returns>
+        public static byte[] Encrypt(string data, string pass, Encoding encoding) {
+            if (string.IsNullOrEmpty(data)) throw new ArgumentNullException("data");
+            if (string.IsNullOrEmpty(pass)) throw new ArgumentNullException("pass");
+
+            return encrypt(encoding.GetBytes(data), encoding.GetBytes(pass));
+        }
+        /// <summary>
+        /// Encrypt
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="pass"></param>
+        /// <returns></returns>
+        public static byte[] Encrypt(string data, string pass) {
+            if (string.IsNullOrEmpty(data)) throw new ArgumentNullException("data");
+            if (string.IsNullOrEmpty(pass)) throw new ArgumentNullException("pass");
+
+            return encrypt(Encoding.UTF8.GetBytes(data), Encoding.UTF8.GetBytes(pass));
+        }
+        /// <summary>
+        /// Encrypt
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="pass"></param>
+        /// <returns></returns>
+        public static byte[] Encrypt(byte[] data, string pass, Encoding encoding) {
+            if (data == null) throw new ArgumentNullException("data");
+            if (data.Length == 0) throw new ArgumentNullException("data");
+            if (string.IsNullOrEmpty(pass)) throw new ArgumentNullException("pass");
+
+            return encrypt(data, encoding.GetBytes(pass));
+        }
+        /// <summary>
+        /// Encrypt
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="pass"></param>
+        /// <returns></returns>
+        public static byte[] Encrypt(byte[] data, string pass) {
+            if (data == null) throw new ArgumentNullException("data");
+            if (data.Length == 0) throw new ArgumentNullException("data");
+            if (string.IsNullOrEmpty(pass)) throw new ArgumentNullException("pass");
+
+            return encrypt(data, Encoding.UTF8.GetBytes(pass));
+        }
+        /// <summary>
+        /// Encrypt
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="pass"></param>
+        /// <returns></returns>
+        public static byte[] Encrypt(string data, byte[] pass) {
+            if (string.IsNullOrEmpty(data)) throw new ArgumentNullException("data");
+            if (pass == null) throw new ArgumentNullException("pass");
+            if (pass.Length == 0) throw new ArgumentNullException("pass");
+
+            return encrypt(Encoding.UTF8.GetBytes(data), pass);
+        }
+        /// <summary>
+        /// Encrypt
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="pass"></param>
+        /// <param name="encoding"></param>
+        /// <returns></returns>
+        public static byte[] Encrypt(string data, byte[] pass, Encoding encoding) {
+            if (string.IsNullOrEmpty(data)) throw new ArgumentNullException("data");
+            if (pass == null) throw new ArgumentNullException("pass");
+            if (pass.Length == 0) throw new ArgumentNullException("pass");
+
+            return encrypt(encoding.GetBytes(data), pass);
+        }
+        /// <summary>
+        /// Encrypt
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="pass"></param>
+        /// <returns></returns>
+        public static byte[] Encrypt(byte[] data, byte[] pass) {
+            if (data == null) throw new ArgumentNullException("data");
+            if (data.Length == 0) throw new ArgumentNullException("data");
+            if (pass == null) throw new ArgumentNullException("pass");
+            if (pass.Length == 0) throw new ArgumentNullException("pass");
+            return encrypt(data, pass);
+        }
+
+        private static byte[] encrypt(byte[] data, byte[] pass) {
+            if (data == null) throw new ArgumentNullException("data");
+            if (pass == null) throw new ArgumentNullException("pass");
+
+            byte[] mBox = GetKey(pass, keyLen);
+            byte[] output = new byte[data.Length];
+            int i = 0, j = 0;
+            for (Int64 offset = 0; offset < data.Length; offset++) {
+                i = (++i) & 0xFF;
+                j = (j + mBox[i]) & 0xFF;
+                byte temp = mBox[i];
+                mBox[i] = mBox[j];
+                mBox[j] = temp;
+
+                byte a = data[offset];
+                byte b = mBox[(mBox[i] + mBox[j]) & 0xFF];
+                output[offset] = (byte)((int)a ^ (int)b);
+            }
+            return output;
+        }
+
+
+        private static byte[] GetKey(byte[] pass, int kLen) {
+            byte[] mBox = new byte[kLen];
+            for (Int64 i = 0; i < kLen; i++) {
+                mBox[i] = (byte)i;
+            }
+            Int64 j = 0;
+            for (Int64 i = 0; i < kLen; i++) {
+                j = (j + mBox[i] + pass[i % pass.Length]) % kLen;
+                byte temp = mBox[i];
+                mBox[i] = mBox[j];
+                mBox[j] = temp;
+            }
+            return mBox;
+        }
+    }
+}
