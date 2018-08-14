386c82cbcfc84bbbc1e73232f074f1c8c20280a6
yiv <yuefengshao@gmail.com>
2018-08-14 16:27:23 +0800

    update

---
+++ protobuf/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Msgpb;
+using System.IO;
+using Google.Protobuf;
+
+namespace protobuf
+{
+    class Program {
+        static void Main(string[] args) {
+            byte[] bytes = MarshalToBytes();
+            UnmarshalFromBytes(bytes);
+
+            Console.ReadKey();
+        }
+
+        static byte[] MarshalToBytes() {
+            byte[] bytes;
+            ConnectReq connectReq = new ConnectReq { Uid = 55555 };
+            using (MemoryStream stream = new MemoryStream()) {
+                connectReq.WriteTo(stream);
+                bytes = stream.ToArray();
+
+            }
+            string str = connectReq.ToString();
+
+            foreach (var i in bytes) {
+                Console.WriteLine(i);
+            }
+            return bytes;
+        }
+        static void UnmarshalFromBytes(byte[] bytes) {
+            ConnectReq connectReq = ConnectReq.Parser.ParseFrom(bytes);
+            Console.WriteLine(connectReq.Uid);
+        }
+    }
+}
